0a129cbdd8d92d4afc4fdbb64e88a0e1e2241bfe
PhuNguyen12345 <phukak12345@gmail.com>
2026-01-29 09:11:29 +0700

    Initial commit

---
+++ src/main/java/com/example/localpos/LocalposApplication.java
@@ -0,0 +1,13 @@
+package com.example.localpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/localpos/LocalposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
