9c931956bf759eae303949a9cad3a31ec283bbb7
PhuNguyen12345 <phukak12345@gmail.com>
2026-03-03 09:31:32 +0700

    feat: DTO for Supplier

--- src/main/java/com/example/localpos/modules/inventory/dto/request/SuppllierRequest.java
+++ src/main/java/com/example/localpos/modules/inventory/dto/request/SuppllierRequest.java
@@ -1,4 +1,15 @@
 package com.example.localpos.modules.inventory.dto.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SuppllierRequest {
+    private Long id;
+    private String name;
+    private String contactPerson;
+    private String phone;
+    private String address;
 }
--- src/main/java/com/example/localpos/modules/inventory/dto/response/SupplierResponse.java
+++ src/main/java/com/example/localpos/modules/inventory/dto/response/SupplierResponse.java
@@ -1,4 +1,15 @@
 package com.example.localpos.modules.inventory.dto.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SupplierResponse {
+    private String name;
+    private String contactPerson;
+    private String phone;
+    private String address;
 }
