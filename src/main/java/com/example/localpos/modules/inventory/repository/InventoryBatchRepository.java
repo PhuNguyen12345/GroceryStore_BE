0c27ef5dde59a3ac54dd91b6539b587745a49323
PhuNguyen12345 <phukak12345@gmail.com>
2026-03-12 12:00:03 +0700

    feat: Update dev for resolving conflict from hieupd15.

--- src/main/java/com/example/localpos/modules/inventory/repository/InventoryBatchRepository.java
+++ src/main/java/com/example/localpos/modules/inventory/repository/InventoryBatchRepository.java
@@ -1,12 +1,19 @@
 package com.example.localpos.modules.inventory.repository;
 
 import com.example.localpos.modules.inventory.entity.InventoryBatch;
+import com.example.localpos.modules.product.entity.ProductUnit;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InventoryBatchRepository extends JpaRepository<InventoryBatch,Long>,
         JpaSpecificationExecutor<InventoryBatch> {
+    List<InventoryBatch> findByProductUnitAndQuantityAvailableGreaterThanOrderByCreatedAtAsc(
+            ProductUnit productUnit,
+            Integer quantity
+    );
 }
