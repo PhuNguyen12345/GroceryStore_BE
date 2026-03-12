ada65e9279b0d717d3aec5bebe43392af36a17fb
PhuNguyen12345 <phukak12345@gmail.com>
2026-03-12 11:48:36 +0700

    feat: Update batch specification

--- src/main/java/com/example/localpos/modules/inventory/repository/specification/BatchSpecification.java
+++ src/main/java/com/example/localpos/modules/inventory/repository/specification/BatchSpecification.java
@@ -1,6 +1,8 @@
 package com.example.localpos.modules.inventory.repository.specification;
 
 import com.example.localpos.modules.inventory.entity.InventoryBatch;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
@@ -10,4 +12,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BatchSpecification {
+    public static Specification filterBatches(
+            String batchCode,
+            String productName,
+            String warehouseName,
+            String supplierName,
+            LocalDate fromExpiryDate,
+            LocalDate toExpiryDate
+    ) {
+        return (root, query, cb) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            //1. Filter for batch code
+            if (StringUtils.hasText(batchCode)) {
+                predicates.add(cb.like(cb.lower(root.get("batchCode")), "%" + batchCode.toLowerCase() + "%"));
+            }
+            if (StringUtils.hasText(productName)) {
+                Join<Object, Object> productUnitJoin = root.join("productUnit", JoinType.INNER);
+                Join<Object, Object> productJoin = productUnitJoin.join("product", JoinType.INNER);
+                predicates.add(cb.like(cb.lower(productJoin.get("name")), "%" + productName.toLowerCase() + "%"));
+            }
+            if (StringUtils.hasText(warehouseName)) {
+                Join<Object, Object> warehouseJoin = root.join("warehouse", JoinType.INNER);
+
+            }
+        }
+    }
 }
